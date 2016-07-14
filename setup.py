61d5c389242baeee2c4afb4973dc8cf0e9342fa1
Yarvis Hatsa <PoliceTerror@dyne.org>
2016-07-14 06:17:47 -0400

    setup.py for pip installer

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "python-libbitcoin",
+    version = "0.1",
+    author = "DarkWallet",
+    author_email = "policeterror@dyne.org",
+    description = ("Python client side library for libbitcoin-server."),
+    license = "AGPL",
+    keywords = "bitcoin library",
+    url = "https://github.com/rojavacrypto/python-libbitcoin",
+    packages=["libbitcoin"],
+    install_requires=["pyzmq"],
+    long_description=read('README.md'),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Security :: Cryptography",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+    ],
+)
+
