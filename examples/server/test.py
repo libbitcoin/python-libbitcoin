d367b55d0e27370eb18df81f46bba24a9237c4cc
Yarvis Hatsa <PoliceTerror@dyne.org>
2016-10-22 11:30:08 +0100

    added raw zmq fetch last height test

---
+++ examples/server/test.py
@@ -0,0 +1,39 @@
+import random
+import struct
+import sys
+import zmq
+
+def create_random_id():
+    MAX_UINT32 = 4294967295
+    return random.randint(0, MAX_UINT32)
+
+url = "tcp://gateway.unsystem.net:9091"
+
+c = zmq.Context()
+
+s = c.socket(zmq.DEALER)
+s.connect(url)
+
+command = b"blockchain.fetch_last_height"
+ident = create_random_id()
+data = b""
+
+request = [
+    command,
+    struct.pack('<I', ident),
+    data
+]
+s.send_multipart(request)
+
+response = s.recv_multipart()
+
+command, response_ident, data = response
+assert struct.unpack('<I', response_ident)[0] == ident
+error = struct.unpack('<I', data[:4])[0]
+if error:
+    print("Error returning height", file=sys.stderr)
+    sys.exit(-1)
+
+height = struct.unpack('<I', data[4:])[0]
+print("Height:", height)
+
