43fc0b43365081d5d84e030c5b7076478e65b7cb
Yarvis Hatsa <PoliceTerror@dyne.org>
2017-01-08 15:15:31 +0000

    missing import of Client in libbitcoin/server/__init__.py

--- libbitcoin/server/__init__.py
+++ libbitcoin/server/__init__.py
@@ -1,5 +1,5 @@
 from libbitcoin.server.bitcoin_utils import bitcoin_hash
-from libbitcoin.server.client import ClientSettings
+from libbitcoin.server.client import ClientSettings, Client
 from libbitcoin.server.context import Context, TornadoContext
 from libbitcoin.server.models import OutPoint, InPoint
 from libbitcoin.server.binary import Binary
