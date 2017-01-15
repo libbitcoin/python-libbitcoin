37c784f3afd36e1dbbc8812ec437b13831bc07be
Yarvis Hatsa <PoliceTerror@dyne.org>
2017-01-15 19:59:47 +0000

    additional script typedefs

--- libbitcoin/bc/headers/script.h
+++ libbitcoin/bc/headers/script.h
@@ -1,5 +1,7 @@
 typedef struct bc_data_chunk_t bc_data_chunk_t;
 typedef struct bc_data_stack_t bc_data_stack_t;
+typedef struct bc_endorsement_t bc_endorsement_t;
+typedef struct bc_ec_secret_t bc_ec_secret_t;
 typedef struct bc_ec_signature_t bc_ec_signature_t;
 typedef struct bc_error_code_t bc_error_code_t;
 typedef struct bc_hash_digest_t bc_hash_digest_t;
